7bdcf3c4df30b53116faf511cdff0a1f9f5aef3e
taify <taifyn932802@gmail.com>
2025-12-15 00:37:00 +0300

    All functions have been implemented. Everything is ready for inspection.

--- catalog/models.py
+++ catalog/models.py
@@ -2,19 +2,38 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
+
 class Category(models.Model):
+    """
+Product category model.
+
+Defines the name and description for each category.
+"""
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
+
     # created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания') # Закомментировано, т.к. не требуется в задании
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Category object.
+        """
         return f'{self.name}'
 
     class Meta:
+        """
+        Inner class for configuring the Category model.
+        """
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+
 class Product(models.Model):
+    """
+    Product model.
+
+    Defines all product characteristics, including its relationship to the category.
+    """
     name = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
     image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение (превью)')
@@ -23,9 +42,15 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Product object.
+        """
         return f'{self.name} ({self.category})'
 
     class Meta:
+        """
+        Inner class for configuring the Product model.
+        """
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'

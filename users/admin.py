a2a0f7ca9f6cb762a63e3eb06bb1f30b64eac3cf
taify <taifyn932802@gmail.com>
2026-01-07 22:05:26 +0300

    The users application and custom user model have been created. The database has been recreated. The superuser has been recreated. Migrations have been reapplied for all applications in the project.

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'country', 'phone', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
